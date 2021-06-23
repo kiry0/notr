0aa745e0e7593985768147e49a391d4fbaa663f8
Kiryū <61569361+shinoa-hiragi@users.noreply.github.com>
2021-06-23 17:07:39 +0200

    :bug: bug fix: causing the route to not resolve

--- src/routes/auth/register.js
+++ src/routes/auth/register.js
@@ -11,32 +11,28 @@ const { v4: uuidv4 } = require('uuid');
 
 router.post('/api/v1/auth/register', async (req, res) => {
     const { email, username, password } = req.body;
-
+    
     if(!email || !username || !password) return res.sendStatus(400);
 
-    try {
-        const hashedPassword = await bcrypt.hash(password, 16);
+    const hashedPassword = await bcrypt.hash(password, 16);
     
-        const user = await User.findOne({ username });
+    const user = await User.findOne({ username });
 
-        if(!user) {
-            const user = new User({
-                email,
-                username,
-                password: hashedPassword,
-                id: uuidv4(),
-                token: crypto.randomBytes(128).toString('hex')
-            });
-    
-            user.save();
+    if(!user) {
+        const user = new User({
+            email,
+            username,
+            password: hashedPassword,
+            id: uuidv4(),
+            token: crypto.randomBytes(128).toString('hex')
+        });
 
-            return res.sendStatus(201);
-        };
-    } catch (e) {
-        console.error(e);
-
-        res.sendStatus(500);
+        user.save();
+        
+        return res.sendStatus(201);
     };
+
+    res.sendStatus(409);
 });
 
 module.exports = router;
