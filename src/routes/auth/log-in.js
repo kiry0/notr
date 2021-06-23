4631ad5935ac070a6c4ca84bb8709aabc28d91b9
Kiryū <61569361+shinoa-hiragi@users.noreply.github.com>
2021-06-23 17:20:43 +0200

    :bug: causing `user.password` to actually be undefined.

--- src/routes/auth/log-in.js
+++ src/routes/auth/log-in.js
@@ -11,20 +11,14 @@ router.post('/api/v1/auth/log-in', async (req, res) => {
     const { email, username, password } = req.body;
 
     if(!email || !username || !password) return res.sendStatus(400);
+    
+    const user = await User.find({ $or:[{ email }, { username }]});
+        
+    if(!user) return res.sendStatus(404);
 
-    try {
-        const user = await User.find({ $or:[{ email }, { username }]});
+    if(!await bcrypt.compare(password, user[0].password)) return res.sendStatus(401);
 
-        if(!user) return res.sendStatus(404);
-
-        if(!await bcrypt.compare(password, user.password)) return res.sendStatus(401);
-
-        res.sendStatus(200);
-    } catch (err) {
-        console.error(err);
-
-        res.sendStatus(500);
-    };
+    res.sendStatus(200);
 });
 
 module.exports = router;
