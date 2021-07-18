4a2247ec8cbd907972d4efd40f49840c6e258dc7
Kiryū <61569361+shinoa-hiragi@users.noreply.github.com>
2021-07-18 14:02:26 +0200

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,8 +1,16 @@
-/* DEPENDENCIES: */
+/* DEPENDENCIES */
 const mongoose = require('mongoose');
 /* */
 
 const userSchema = mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
     email: {
         type: String,
         required: true,
