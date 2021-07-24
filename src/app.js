1c083511ca47736004ded0e41e49786dd0cbe5c1
Kiryū <61569361+shinoa-hiragi@users.noreply.github.com>
2021-07-24 18:06:49 +0200

    :recycle: slight refactor

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const Server = require('./lib/classes/Server.js');
 /* */
 
 const server = new Server({
-    mongodbURI: `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@dev.yt6e2.mongodb.net/Notr`
+    mongodbURI: process.env.MONGODB_URI
 });
 
 server.start();
